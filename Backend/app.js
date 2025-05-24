b19c9e36831e17df6bfe821f86e373fb67f24fe9
Ritin rawat <ritinrawat378@gmail.com>
2025-05-24 10:41:17 +0530

    change

--- Backend/app.js
+++ Backend/app.js
@@ -28,10 +28,6 @@ app.use('/captains',captainRoutes)
 app.use('/maps',mapRoutes)
 app.use('/rides',rideRoutes)
 
-app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
-});
 
 module.exports=app
